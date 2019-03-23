e69bc8757092766f790dfab0bd2950ee670a39a9
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-03-23 23:48:57 +0800

    script -> lualib

---
+++ lualib/publish/conf/beta.lua
@@ -0,0 +1,29 @@
+local util = require "bw.util"
+local conf = require "conf"
+conf = util.clone(conf)
+conf.desc = "share(测试服)"
+conf.remote_host = "www@kid-dev.xqw369.com"
+conf.remote_port = 22
+conf.remote_path = "/mnt/dev/server/share"
+conf.etcname = "shared"
+conf.clustername = "share"
+conf.cluster = {
+    share   = "127.0.0.1:19901",
+}
+conf.mongo = {
+    host = "127.0.0.1",
+    name = "xqw_beta",
+    port = 19900,
+    user = "xqw",
+    password = "1qaz2wsx",
+}
+conf.webconsole = {
+    host = "kid-dev.xqw369.com",
+    port = 9997,
+}
+conf.debug_console = {
+    host = "kid-dev.xqw369.com",
+    port = 9996,
+}
+conf.alert.enable = true
+return conf
---
+++ lualib/publish/conf/release.lua
@@ -0,0 +1,29 @@
+local util = require "bw.util"
+local conf = require "conf"
+conf = util.clone(conf)
+conf.desc = "share(正式服)"
+conf.remote_host = "www@kid.xqw369.com"
+conf.remote_port = 22
+conf.remote_path = "/mnt/data/server/share"
+conf.etcname = "shared"
+conf.clustername = "share"
+conf.cluster = {
+    share   = "127.0.0.1:19801",
+}
+conf.mongo = {
+    host = "127.0.0.1",
+    name = "xqw_beta",
+    port = 19800,
+    user = "xqw",
+    password = "1qaz2wsx",
+}
+conf.webconsole = {
+    host = "kid.xqw369.com",
+    port = 9897,
+}
+conf.debug_console = {
+    host = "kid.xqw369.com",
+    port = 9896,
+}
+conf.alert.enable = true
+return conf
